374ea92970e5e317e137783ab89dbdc41ab78307
Akiya <79969591+luyeeeee@users.noreply.github.com>
2024-11-07 12:18:40 +0800

    Update trojan.lua

--- trojan.lua
+++ trojan.lua
@@ -155,6 +155,8 @@ local function doDissect(tvb, pktinfo, root)
         pktinfo.port_type = save_port_type
         pktinfo.can_desegment = save_can_desegment
 
+        pktinfo.cols.protocol:set(PROTOCOL_NAME)
+
         ---
         --- The following code chunk gives many Continuation on HTTP/1.1
 
